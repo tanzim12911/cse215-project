27be5e786169c5264af1a77eb17d563d327f78c5
Mir-37 <mirnihal37@gmail.com>
2023-11-13 05:07:10 +0600

    rename projectBkash to Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package projectbkash;
+package main;
 
 import controller.src.Client;
 import java.io.IOException;
@@ -14,7 +14,7 @@ import java.util.Scanner;
  *
  * @author hellm
  */
-public class ProjectBkash {
+public class Main {
 
     /**
      * @param args the command line arguments
