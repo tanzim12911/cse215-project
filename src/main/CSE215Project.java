93ceb9976ace740370cf4f1cc776171d57aa58c4
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-11-17 23:38:19 +0600

    Add files via upload
    Initial GUI Files

---
+++ src/main/CSE215Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cse215.project;
+
+/**
+ *
+ * @author tanzi
+ */
+public class CSE215Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new WelcomePage().setVisible(true);
+    }
+    
+}
---
+++ src/main/Client.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cse215.project;
+
+/**
+ *
+ * @author tanzi
+ */
+public class Client {
+    private String phoneNum;
+    private String pin;
+    private String Name;
+    private double balance;
+
+    public Client(String phoneNum, String pin, String Name, double balance) {
+        this.phoneNum = phoneNum;
+        this.pin = pin;
+        this.Name = Name;
+        this.balance = balance;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    
+}
